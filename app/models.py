46c5395f4e56f617fc62050bfb30280e7e36f19a
Divine Mukoro <surbvinex@gmail.com>
2024-03-26 14:17:44 +0100

    Added Follower feature 01
    Added many to many relationship in models for users followers and following feature

--- app/models.py
+++ app/models.py
@@ -13,6 +13,12 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class Follow(db.Model):
+    __tablename__ = "follows"
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +37,17 @@ class User(db.Model, UserMixin):
     avatar_hash = db.Column(db.String(32))
 
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # TODO: read the sqlalchemy documentation on database relationship and postgressql relationship.
+    followed = db.relationship('Follow',
+                               foreign_keys=[Follow.follower_id],
+                               backref=db.backref('follower', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
+    followers = db.relationship('Follow',
+                               foreign_keys=[Follow.followed_id],
+                               backref=db.backref('followed', lazy='joined'),
+                               lazy='dynamic',
+                               cascade='all, delete-orphan')
 
     def __repr__(self):
         return f"User('{self.username}', {self.email}', {self.image_file}')"
@@ -96,6 +113,26 @@ class User(db.Model, UserMixin):
         if not (user_id := data.get("user_id")):
             return None
         return User.query.get(user_id)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(follower=self, followed=user)
+            db.session.add(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            db.session.delete(f)
+    
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(followed_id=user.id).first() is not None
+    
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(follower_id=user.id).first() is not None
 
 
 class Post(db.Model):
@@ -107,3 +144,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"Post('{self.author.username}', {self.timestamp})"
+
